3e7c60326a83bf054a14c48a386ead9d2551d7eb
Sherlock <vadsan27200@gmail.com>
2017-02-02 19:51:45 +0200

    some bad commit with idea

--- Homework/src/arrayList/Run.java
+++ Homework/src/arrayList/Run.java
@@ -6,21 +6,23 @@ package arrayList;
 public class Run {
     public static void main(String[] args) {
         ArrayList car = new ArrayList(1);
-
+        System.out.println("add index:");
         car.add("Honda");
         car.add("Mazda");
         car.add("Seat");
         car.add("BMW");
 
         car.showList();
-
+        System.out.println("Add index and object: ");
         car.add(5, "Audi");
         car.showList();
 
         System.out.println(car.get(5));
+        System.out.println("Remove index:");
         car.remove(0);
         car.showList();
-        car.set(3, "");
+        System.out.println("Set mark car:");
+        car.set(5, "Lada");
         car.showList();
 
         System.out.println(car.contains("Seat"));
